ad360d09594931f7caa38ec3c066ceba9ea78446
Gregory Grubbs <gregory@dynapse.com>
2025-09-06 11:55:58 -0600

    add a performance param on satellite devices with no GPU

--- bin/restart-browser.sh
+++ bin/restart-browser.sh
@@ -4,6 +4,7 @@ killall -v chromium
 killall -v chromium-browser
 
 DISPLAY=:0 chromium-browser \
+	   --disable-gpu \
            --start-maximized \
            --start-fullscreen \
            http://homeassistant.magichome:8123 &
