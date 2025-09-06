42239799c8240906991eab4d41da9b0018439d83
Gregory Grubbs <gregory@dynapse.com>
2025-09-06 11:58:15 -0600

    add more info to log directed to /tmp

--- bin/tally-me-timer.sh
+++ bin/tally-me-timer.sh
@@ -5,3 +5,8 @@ thing=$(cat)
 
 echo "$(date)  timer-start: This is the full command: $@" | tee -a /tmp/timer-started.log
 echo "                                       and cat: $thing"          | tee -a /tmp/timer-started.log
+
+echo "   ID is $(echo $thing | jq -r '.data.id')" | tee -a /tmp/timer-started.log
+echo "   Name is $(echo $thing | jq -r '.data.name')" | tee -a /tmp/timer-started.log
+
+echo "$(echo $thing | jq -r '.data.name')" > /tmp/$(echo $thing | jq -r '.data.id').timer
